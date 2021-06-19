192e55638d713f1f5a79ee5740aeff42074a41fc
hui3678 <82255334+hui3678@users.noreply.github.com>
2021-06-19 19:09:08 +0800

    Add files via upload

---
+++ Debug/X-CUBE-AI/App/digit_recognition_model_data.d
@@ -0,0 +1,11 @@
+X-CUBE-AI/App/digit_recognition_model_data.o: \
+ ../X-CUBE-AI/App/digit_recognition_model_data.c \
+ ../X-CUBE-AI/App/digit_recognition_model_data.h \
+ ../X-CUBE-AI/App/digit_recognition_model_config.h \
+ ../Middlewares/ST/AI/Inc/ai_platform.h
+
+../X-CUBE-AI/App/digit_recognition_model_data.h:
+
+../X-CUBE-AI/App/digit_recognition_model_config.h:
+
+../Middlewares/ST/AI/Inc/ai_platform.h:
---
+++ Debug/X-CUBE-AI/App/subdir.mk
@@ -0,0 +1,25 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../X-CUBE-AI/App/digit_recognition_model.c \
+../X-CUBE-AI/App/digit_recognition_model_data.c 
+
+OBJS += \
+./X-CUBE-AI/App/digit_recognition_model.o \
+./X-CUBE-AI/App/digit_recognition_model_data.o 
+
+C_DEPS += \
+./X-CUBE-AI/App/digit_recognition_model.d \
+./X-CUBE-AI/App/digit_recognition_model_data.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+X-CUBE-AI/App/digit_recognition_model.o: ../X-CUBE-AI/App/digit_recognition_model.c X-CUBE-AI/App/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F446xx -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/ST/AI/Inc -I../X-CUBE-AI/App -I"C:/Users/User/STM32CubeIDE/Digit_Recognition/Digit_Recognition/stb_image/inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"X-CUBE-AI/App/digit_recognition_model.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+X-CUBE-AI/App/digit_recognition_model_data.o: ../X-CUBE-AI/App/digit_recognition_model_data.c X-CUBE-AI/App/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F446xx -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/ST/AI/Inc -I../X-CUBE-AI/App -I"C:/Users/User/STM32CubeIDE/Digit_Recognition/Digit_Recognition/stb_image/inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"X-CUBE-AI/App/digit_recognition_model_data.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
